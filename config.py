2a499286dcfa793f5dc60685f638fe188d144c2c
bongodon <yephtipjar@gmail.com>
2025-02-01 23:14:45 +0000

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,3 @@ ghPagesURL = "https://bongodon.github.io/tiktok-rss-flat/"
 ghRawURL = "https://raw.githubusercontent.com/conoro/tiktok-rss-flat/main/"
 
 # ghPagesURL = "https://tiktokrss.conoroneill.com/"
-
-https://github.com/bongodon/tiktok-rss-flat
